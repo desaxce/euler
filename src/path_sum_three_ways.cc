f4b11d0d977fbc2146bbdeb0a2bf05b3cb4fd6aa
goemon <hughes.desaxce@gmail.com>
2014-09-27 13:24:06 +0200

    Solved Path sum three ways - pb 82

--- src/path_sum_three_ways.cc
+++ src/path_sum_three_ways.cc
@@ -4,42 +4,87 @@
 #include <deque>
 
 #define MIN(a, b)   ((a) < (b) ? (a): (b))
-#define N  5
+#define N  80
 
 using namespace std;
 
 deque<deque<int> > matrix(N);
-deque<deque<int> > memoizer(N);
+deque<int> table(N);
+deque<int> nextTable(N);
 
-int rec(int n, int m) {
-    if (memoizer[n][m] != 0) {
-        return memoizer[n][m];
-    }
-    if (n == N-1) {
-        memoizer[n][m] = 0;
-        for (int i = m; i < N; ++i) {
-            memoizer[n][m] += matrix[N-1][i];
+int min(int k, int n) {
+    int current;
+    int result = matrix[k][n] + table[k];
+    
+    // Partie inferieure a k
+    current = matrix[k][n] + table[k];
+    for (int i = k-1; i > -1; --i) {
+        current += -table[i+1]+matrix[i+1][n+1]+table[i];  
+        if (current < result) {
+            result = current;
         }
-        return memoizer[n][m];
     }
-    if (m == N-1) {
-        memoizer[n][m] = 0;
-        for (int i = n; i < N; ++i) {
-            memoizer[n][m] += matrix[i][N-1];
+
+    // Partie superieure a k
+    current = matrix[k][n] + table[k];
+    for (int i = k+1; i < N; ++i) {
+        current += -table[i-1]+matrix[i-1][n+1]+table[i];
+        if (current < result) {
+            result = current;
         }
-        return memoizer[n][m];
     }
-    memoizer[n][m] = matrix[n][m]+MIN(rec(n+1, m), rec(n, m+1));
-    return memoizer[n][m];
+    
+    // == k
+    if (matrix[k][n]+table[k] < result) {
+        result = matrix[k][n]+table[k];
+    }
+
+    return result;
+}
+
+void loop() {
+    // Initialisation
+    for (int i = 0; i < N; ++i) {
+        table[i] = matrix[i][N-1];
+        // printf("%d ", table[i]);
+    }
+    // printf("\n");
+
+    // Attention a l'indicage sur n (on va de droite (la fin) a gauche)
+    for (int n = N-2; n > -1; --n) {
+        // Pas tres beau d'utiliser deux tableaux
+        // A changer si possible (faire les modifications en place)
+        for (int k = 0; k < N; ++k) {
+            nextTable[k] = min(k, n);
+        }
+        for (int k = 0; k < N; ++k) {
+            table[k] = nextTable[k];
+            // printf("%d ", table[k]);
+        }
+        // printf("\n");
+    }
 }
 
 int main() {
+    // Reads from stdin
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    printf("%d\n", rec(0, 0));
+
+    // Computes the minimal paths in |table|
+    loop();
+    
+    // Takes the minimal path among the N paths in |table|
+    int result = table[0];
+    for (int i = 0; i < N; ++i) {
+        if (table[i] < result) {
+            result = table[i];
+        }
+    }
+    printf("min path sum = %d\n", result);
+
     return 0;
 }
 
