83a4a8f6ae311251856a26b87f2b9d44cb194ed6
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-02-16 20:42:13 +0100

    Solved Path sum four ways with a graph (Dijkstra)

--- src/path_sum_four_ways.cc
+++ src/path_sum_four_ways.cc
@@ -2,35 +2,57 @@
 #include <stdlib.h>
 #include <iostream>
 #include <deque>
+#include <unordered_map>
+#include <queue>
 
 #define MIN(a, b)   ((a) < (b) ? (a): (b))
-#define N  5
+#define N 80 
+#define BILLION 1000000000
 
 using namespace std;
 
-deque<deque<int> > matrix(N);
-deque<deque<int> > memoizer(N);
+// Modified input file (replaced commas with spaces, cause it's easier to parse this way)
 
-int rec(int n, int m) {
-    if (memoizer[n][m] != 0) {
-        return memoizer[n][m];
-    }
-    if (n == N-1) {
-        memoizer[n][m] = 0;
-        for (int i = m; i < N; ++i) {
-            memoizer[n][m] += matrix[N-1][i];
-        }
-        return memoizer[n][m];
-    }
-    if (m == N-1) {
-        memoizer[n][m] = 0;
-        for (int i = n; i < N; ++i) {
-            memoizer[n][m] += matrix[i][N-1];
-        }
-        return memoizer[n][m];
-    }
-    memoizer[n][m] = matrix[n][m]+MIN(rec(n+1, m), rec(n, m+1));
-    return memoizer[n][m];
+deque<deque<int> > matrix(N);
+deque<deque<int> > graph(N*N);
+
+struct compare {
+	bool operator()(const int& l, const int& r) {
+		return l > r;
+	}
+};
+
+int dijkstra() {
+	queue<int> f;
+	f.push(0);
+	int result = BILLION;
+	int mark[N*N];
+	int distance[N*N];
+
+	for (int i = 0; i < N*N; ++i) {
+		mark[i] = 0;
+		distance[i] = BILLION;
+	}
+	distance[0] = matrix[0][0];
+
+	while (!f.empty()) {
+		int s = f.front();
+		f.pop();
+		mark[s] = 1;
+
+		for (int i : graph[s]) {
+			if (!mark[i]) {
+				distance[i] = distance[s]+matrix[i/N][i%N];
+				f.push(i);
+				mark[i] = 1;
+			}
+			else if (distance[i] > distance[s]+matrix[i/N][i%N]) {
+				distance[i] = distance[s]+ matrix[i/N][i%N];
+				f.push(i); // We add this to make sure that all distances are well updated
+			}
+		}
+	}
+	return distance[N*N-1];
 }
 
 int main() {
@@ -39,7 +61,64 @@ int main() {
             scanf("%d", &matrix[i][j]);
         }
     }
-    printf("%d\n", rec(0, 0));
-    return 0;
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			deque<int> neighbors;
+			if (i==0) {
+				if (j==0) {
+					neighbors.push_back(1);
+					neighbors.push_back(N);
+				}
+				else if (j==N-1) {
+					neighbors.push_back(N-2);
+					neighbors.push_back(2*N-1);
+				}
+				else {
+					neighbors.push_back(j+1);
+					neighbors.push_back(j-1);
+					neighbors.push_back(N+j);
+				}
+			}
+			else if (i==N-1) {
+				if (j==0) {
+					neighbors.push_back(i*N+1);
+					neighbors.push_back(i*N-N);
+				}
+				else if (j==N-1) {
+					neighbors.push_back(N*i+j-1);
+					neighbors.push_back(N*i+j-N);
+				}
+				else {
+					neighbors.push_back(i*N+j+1);
+					neighbors.push_back(i*N+j-1);
+					neighbors.push_back(i*N+j-N);
+				}
+			}
+			else {
+				if (j==0) {
+					neighbors.push_back(i*N+1);
+					neighbors.push_back(i*N-N);
+					neighbors.push_back(i*N+N);
+				}
+				else if (j==N-1) {
+					neighbors.push_back(i*N+j-1);
+					neighbors.push_back(i*N+j+N);
+					neighbors.push_back(i*N+j-N);
+				}
+				else {
+					neighbors.push_back(i*N+j+1);
+					neighbors.push_back(i*N+j-1);
+					neighbors.push_back(i*N+j+N);
+					neighbors.push_back(i*N+j-N);
+				}
+			}
+			graph[N*i+j] = neighbors;
+		}
+	}
+
+	// call dijkstra on graph
+	cout << dijkstra() << endl;
+	return 0;
 }
 
