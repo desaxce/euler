f7e3d4071830f0f809134ea47eb4830d785c4210
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-02-22 22:34:03 +0100

    WIP

---
+++ src/compromise.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	n = 3;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <=n; ++j) {
+			if (j!=i) {
+				for (int k = 1; k <=n; ++k) {
+					
+				}
+			}
+		}
+	}
+	return 0;
+}
