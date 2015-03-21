ef0666e87e41261278ad5f9c66772b5fa80206ef
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-03-21 14:35:11 +0100

    WIP: idempotents

--- src/idempotents.cc
+++ src/idempotents.cc
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+long long N = 1e5L;
+
 long M(long n) {
 	long a = n-1;
-	while (a*(a-1)%n!=0)
-		--a;
-	return a;
+/bin/bash: q: command not found
+		if (a*(a-1)%n==0)
+			return a;
+	}
+	return 1;
 }
 
 int main() {
-	cout << M(6) << endl;
+	cout << "M(6) = " << M(6) << endl;
 	long long result = 0;
-	for (long n = 1; n < 100000; ++n) {
+	for (long n = 1; n < N; ++n) {
 		result += M(n);
 	}
-	cout << "sigma(M(n)) de 1 à n = " << result << endl;
+	cout << "sigma(M(n)) de 1 à " << N << " = " << result << endl;
 	return 0;
 }
