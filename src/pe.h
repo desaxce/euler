4b72d7864601a07b6a42d3ffde7dcbdfb98c8da5
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-03-22 10:23:21 +0100

    Created pe.h file

---
+++ src/pe.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cmath>
+#include <list>
+#include <vector>
+#include <deque>
+#include <primesieve.hpp>
+
+#define MAX(a, b)	((a) > (b)) ? (a): (b)
+#define MIN(a, b)	((a) > (b)) ? (b): (a)
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+
