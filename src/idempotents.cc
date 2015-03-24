5edd996bc38304ed0bf46b951cf461dcd7469404
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-03-24 15:07:43 +0100

    Solved pb 407: Idempotents, using PARI/GP ~2minutes

--- src/idempotents.cc
+++ src/idempotents.cc
@@ -1,25 +1,26 @@
-#include <iostream>
-#include <cmath>
+#include <pe.h>
+
+#define ll long long
 
 using namespace std;
 
-long long N = 1e5L;
-
-long M(long n) {
-	long a = n-1;
-/bin/bash: q: command not found
-		if (a*(a-1)%n==0)
+ll M(ll n) {
+	ll a = n-1;
+	while (a>0) {
+		if ((a*(a -1))%n==0)
 			return a;
+		--a;
 	}
 	return 1;
 }
 
-int main() {
-	cout << "M(6) = " << M(6) << endl;
-	long long result = 0;
-	for (long n = 1; n < N; ++n) {
+int main(int argc, char* argv[]) {
+	ll N = atoi(argv[1]);
+	/*ll result = 0;
+	for (ll n = 1; n <= N; ++n) {
 		result += M(n);
-	}
-	cout << "sigma(M(n)) de 1 à " << N << " = " << result << endl;
+	}*/
+	cout << "M(" << N << ") = " << M(N) << endl;
+	//cout << "sigma(M(n)) de 1 à " << N << " = " << result << endl;
 	return 0;
 }
--- src/idempotents.gp
+++ src/idempotents.gp
@@ -1,11 +1,39 @@
+maximum(v, n)=
+{
+	local(i, j, res, resultat);
+	resultat = 0;
+	for (i=0, 2^(matsize(v)[2])-1,
+		res = 0;
+		for (j=0, matsize(v)[2]-1,
+			res+=v[j+1]*bitand(2^j, i)/(2^j);
+		);
+		res = res%n;
+		if (res > resultat,
+			resultat = res;
+		);
+	);
+	return (resultat);
+}
+
 M(n)=
 {
-	local(a, result);
-	result = 1;
-	for(a=n/2-1,n-1,
-		if (Mod(a^2,n)==Mod(a,n),
-			result = a;
-		);
+	local(a, p, ni, nic, i);
+	p = factor(n); 
+	v = vector(matsize(p)[1]);
+	for(i=1, matsize(p)[1],
+		ni=(p[i,1])^(p[i,2]);
+		nic=n/ni;
+		v[i] = nic*lift((Mod(nic, ni))^(-1));
+	);
+	return (maximum(v, n));
+}
+
+sig(n)=
+{
+	local(result, i);
+	result = 0; \\ on compte directement le cas i = 1
+	for (i=1, n,
+		result+=M(i);
 	);
 	return (result);
 }
