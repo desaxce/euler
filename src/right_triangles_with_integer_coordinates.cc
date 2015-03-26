3bd7a25019fb5fd855e7de95c4391b6a4b94d7a0
Hugues de Saxcé <hughes.desaxce@gmail.com>
2015-03-26 09:28:19 +0100

    Simplified code pb 91

--- src/right_triangles_with_integer_coordinates.cc
+++ src/right_triangles_with_integer_coordinates.cc
@@ -1,15 +1,13 @@
 #include <pe.h>
 
-ll N = 50;
+ll N = 500;
 
 int main() {
-	ll result = 0;
+	ll result = N*N;
 	for (ll x1 = 0; x1 <= N; ++x1) {
-		for (ll y1 = 0; y1 <= N; ++y1) if (x1!=0 or y1!=0) {
+		for (ll y1 = 0; y1 <= N; ++y1) {
 			for (ll x2 = 0; x2 <= N; ++x2) {
-				for (ll y2 = 0; y2 <= N; ++y2) if (x2!=0 or y2!=0) {
-					if (x1==0 and y2==0) // right angle at (0, 0) 
-						result++;
+				for (ll y2 = 0; y2 <= N; ++y2) if (x1*y2-y1*x2!=0) { // Non trivial triangle
 					if (x1*(x2-x1)+y1*(y2-y1)==0) // right angle at (x1, y1)
 						result++;
 				}
@@ -17,7 +15,7 @@ int main() {
 		}
 	}
 	// Have to remove the cases where (x1, y1)==(x2, y2)
-	cout << "Integered right triangles with N = " << N << " : " << result -(N+1)*(N+1)+ 1 << endl;
+	cout << "Integered right triangles with N = " << N << " : " << result << endl;
 	return 0;
 }
 
