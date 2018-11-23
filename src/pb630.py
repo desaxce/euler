e16fb92b169661fed9c3b4903a84fa17c848ab15
desaxce <hughes.desaxce@gmail.com>
2018-11-23 09:36:30 +0100

    Solved Problem 630 - Crossed lines

--- src/pb630.py
+++ src/pb630.py
@@ -1,36 +1,24 @@
-import itertools
-
-N = 100
-
-def mod(k, n):
-	modulo = k%n
-	if modulo < 0:
-		return modulo+n
-	return modulo
+N = 2500
 
 S = [290797]
 T = []
 j = 1
 while j < 2*N+1:
-	S.append(mod(S[j-1]**2, 50515093))
-	T.append(mod(S[j], 2000)-1000)
+	S.append(S[j-1]**2 % 50515093)
+	T.append((S[j]%2000)-1000)
 	j += 1
-
-#print(T[0], T[1])
-#print(T[2], T[3])
-#print(T[4], T[5])
 
 droites = []
 droitesVerticales = []
+TS = [[T[2*i], T[2*i+1]] for i in range(0, N)]
+print("Nombre de points au départ: ", len(TS))
 
-for i in range(0, N):
-	A = [T[2*i], T[2*i+1]]
-	for j in range(i+1, N):
-		B = [T[2*j], T[2*j+1]]
-		if A[0] == B[0] and A[1] == B[1]:
-			continue
+for i in range(0, len(TS)):
+	A = TS[i]
+	for j in range(i+1, len(TS)):
+		B = TS[j]
 		if A[0] != B[0]:
-			c = (B[1]-A[1])/(B[0]-A[0])
+			c =(B[1]-A[1])/(B[0]-A[0])
 			droites.append([c, A])
 		else:
 			droitesVerticales.append(A[0])
@@ -40,10 +28,10 @@ print("Nombre de droites avec multiplicité:", len(droites))
 
 # Première méthode utilisée via itertools
 # On se restreint aux droites distinctes
-# Ne marche plus pour un grand N à cause d'erreurs de précision sur d
 # droitesDistinctes = list(droites for droites,_ in itertools.groupby(droites))
 
-# Deuxième méthod en gardant un point A(x,y) appartenant à la droite
+# Deuxième méthode
+# On garde un point A(x,y) appartenant à la droite
 # et non pas spécifiquement l'abscisse à l'origine (erreurs d'arrondis)
 
 # Toutes les droites verticales avec multiplicité
@@ -68,13 +56,18 @@ nombreDroitesDistinctes = 0
 for c in d.keys():
 	points = d[c]
 	e[c] = len(points)
-	for i in range(0, len(points)):
+	for i in range(0, len(points)-1):
+		A = points[i]
 		for j in range(i+1, len(points)):
-			if points[i][0] == points[j][0]: # C'est le même point
+			B = points[j]
+			#if A[0] == B[0] and A[1] == B[1]: # C'est le même point
+			if A == B:
 				e[c] -= 1
 				break
 			else:
-				nouveauCoefficient = (points[j][1]-points[i][1])/(points[j][0]-points[i][0])
+				if A[0] == B[0]:
+					continue
+				nouveauCoefficient = (B[1]-A[1])/(B[0]-A[0])
 				if c == nouveauCoefficient: # i.e. les deux points sont sur la même droite
 					e[c] -= 1
 					break
